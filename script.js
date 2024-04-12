7130165d9da43b8e583133ebb5141c6f34a24e79
sajibtalukder2k16 <sajibtalukder2k16@gmail.com>
2024-04-12 19:48:44 -0400

    research interest and background image

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Fast Learner","Programmer",  "Problem Solver", "Developer","Engineer"],
+        strings: ["Researcher", "Fast Learner", "Programmer", "Problem Solver", "Developer","Engineer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Fast Learner","Programmer","Problem Solver", "Developer","Engineer"],
+        strings: ["Researcher", "Fast Learner","Programmer","Problem Solver", "Developer","Engineer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
